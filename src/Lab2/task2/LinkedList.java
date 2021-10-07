44c96a6e5408118cd244f515c3fa7fc947af0c73
maksdaniluk140@gmail.com <maksym.danyliuk.itis.2020@lpnu.ua>
2021-10-07 22:03:13 +0300

    Changed clear method. Replaced NullPointerException with RuntimeException

--- src/Lab2/task2/LinkedList.java
+++ src/Lab2/task2/LinkedList.java
@@ -1,5 +1,7 @@
 package Lab2.task2;
 
+import java.util.EmptyStackException;
+
 public class LinkedList<T> implements Stack<T> {
     protected Node<T> top;
 
@@ -19,9 +21,9 @@ public class LinkedList<T> implements Stack<T> {
     }
 
     @Override
-    public T pop() throws NullPointerException {
+    public T pop() throws RuntimeException{
         if (empty()) {
-            throw new NullPointerException("Stack is empty");
+            throw new RuntimeException("Stack is empty");
         }
 
         final Node<T> topPointer = top;
@@ -30,18 +32,18 @@ public class LinkedList<T> implements Stack<T> {
     }
 
     @Override
-    public T peek() throws NullPointerException {
+    public T peek() throws RuntimeException {
         if (empty()) {
-            throw new NullPointerException("Stack is empty");
+            throw new RuntimeException("Stack is empty");
         }
 
         return top.getValue();
     }
 
     @Override
-    public int search(final T value) throws NullPointerException {
+    public int search(final T value) throws RuntimeException {
         if (empty()) {
-            throw new NullPointerException("Stack is empty");
+            throw new RuntimeException("Stack is empty");
         }
 
         Node<T> topPointer = top;
@@ -59,16 +61,12 @@ public class LinkedList<T> implements Stack<T> {
     }
 
     @Override
-    public void clear() throws NullPointerException {
+    public void clear() throws RuntimeException {
         if (empty()) {
-            throw new NullPointerException("Stack is empty");
+            throw new RuntimeException("Stack is empty");
         }
 
-        while (top != null) {
-            final Node<T> topPointer = top;
-            top = topPointer.getNext();
-        }
-
+        top = null;
     }
 
     @Override
@@ -77,9 +75,9 @@ public class LinkedList<T> implements Stack<T> {
     }
 
     @Override
-    public int size() throws NullPointerException {
+    public int size() throws RuntimeException {
         if (empty()) {
-            throw new NullPointerException("Stack is empty");
+            throw new RuntimeException("Stack is empty");
         }
 
         Node<T> topPointer = top;
